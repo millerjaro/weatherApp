6056fa145ec0c6875ca168fe037eed07097b2ba0
miller <janmillerjaro@gmail.com>
2024-07-04 23:13:21 +0800

    Improve Features

---
+++ script.js
@@ -0,0 +1,35 @@
+<script>
+const apiKey = "4824bb7648c6d65f804dac319f760a04";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%" ;
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+    }
+    else if(data.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clouds.png";
+    }
+
+}
+
+
+
+searchBtn.addEventListener("click", () =>{
+    checkWeather(searchBox.value);
+})
+
+    </script>
